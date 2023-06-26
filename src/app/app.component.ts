8860f5dda98ebe4ceec7f003800bb97997ecf7e7
Krishan Kant Gautam <krishankant349@gmail.com>
2023-06-26 23:32:48 +0530

    adding projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,6 +66,46 @@ export class AppComponent {
     'Conducted thorough testing and debugging, ensuring high-quality and bug-free code.',
     'Actively participated in code reviews and provided constructive feedback to improve overall code quality and maintainability.',
   ];
+
+  projects = [
+    {
+      name: 'Digital Clock',
+      description: 'Digital Clock is like other clock which is created using JavaScript',
+      image: '',
+      url: ''
+    },
+    {
+      name: 'Calculator',
+      description: 'It is just a basic calculator',
+      image: '',
+      url:''
+    },
+    {
+      name: 'To Do',
+      description: 'It is just a pratice project for user to make their daily to do tasks',
+      image: '',
+      url:''
+    },
+    {
+      name: 'Dr Dev',
+      description: 'Dr Dev is doctor based blog website where you can contact with doctor appoinntment',
+      image: '',
+      url:''
+    },
+    {
+      name: 'Quiz App',
+      description: 'It is just a pratice project for user interaction with some basic GK and Math questions',
+      image: '',
+      url:''
+    },
+    {
+      name: 'News App',
+      description: 'It is a simple news application which show news from different news agencies.',
+      image: '',
+      url:''
+    },
+
+  ]
   getSVGImageUrl(svg: any) {
     // console.log(svg);
 
